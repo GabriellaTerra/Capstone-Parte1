4fb541dea64791a9f329d863744d77397975b7e5
GabriellaTerra <102325601+GabriellaTerra@users.noreply.github.com>
2022-05-19 13:44:31 -0300

    Add files via upload

---
+++ m1-entrega-capstone-ecommerce-GabriellaTerra/js/database.js
@@ -0,0 +1,64 @@
+// Banco de dados dos produtos
+
+const data = [
+  {
+    id: 1,
+    img: "..img/perna-prostética-acrilico.jpg",
+    nameItem: "Prótese de acrílico para perna",
+    description:
+      "Adicione um pouco de estilo ao seu dia-a-dia com modelo feito com detalhes em acrílico...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Próteses"],
+  },
+  {
+    id: 2,
+    img: "..img/mão-bionica-3Dprint-rosa.jpg",
+    nameItem: "Prótese mão biônica 3D print",
+    description:
+      "A inovação e acessibilida chegaram! Um olhar no futuro,trazendo a implementação desta técnica revolucionária...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Próteses"],
+  },
+  {
+    id: 3,
+    img: "..img/coleção-de-mãos-bionicas.jpg",
+    nameItem: "Prótese mão biônica",
+    description:
+      "Cada uma dessas próteses é feita com técnologia de ponta e design anatomico...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Próteses"],
+  },
+  {
+    id: 4,
+    img: "..img/knee-cap-heart-shape.jpg",
+    nameItem: "Acessório para Joelho",
+    description:
+      "Esta coleção vem com diferentes formas para anexar a sua prótese e trazer mais estilo ao dia-a-dia...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Acessórios"],
+  },
+  {
+    id: 5,
+    img: "..img/acessorio-extra-dedo-3DPRINT.jpg",
+    nameItem: "Acessório Extra-finger",
+    description:
+      "Produto designado para extensão física, modelo impresso e de fácil adaptação...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Acessórios"],
+  },
+  {
+    id: 6,
+    img: "..img/acessorio-perna-para-corrida.jpg",
+    nameItem: "Prótese anexável para corrida",
+    description:
+      "Modelo de prótese para a perna, para optimização da performance em corridas...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Acessórios"],
+  },
+];
